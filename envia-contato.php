92df052ed75b977b93493ef8fb77b5f3a9500ff1
Felipe Lima <eu@felipelima.com.br>
2024-06-07 17:15:17 -0300

    Update envia-contato.php

--- envia-contato.php
+++ envia-contato.php
@@ -1,6 +1,4 @@
 <?php
-header('Content-Type: text/html; charset=iso-8859-1');
-
 if ( isset( $_POST['envia'] ) && $_POST['envia'] == 'ok' )
 {
 	/*
@@ -47,7 +45,7 @@ if ( isset( $_POST['envia'] ) && $_POST['envia'] == 'ok' )
 	function enviaEmail($nome,$de,$para,$mensagem,$assunto)
 	{
 		$headers= "From: $nome <$de>\n";
-		$headers .= "Content-Type: text/html; charset=utf-8\n";
+		$headers .= "Content-Type: text/html; charset=iso-8859-1\n";
 		$headers .= "MIME-Version: 1.0\r\n";
 		$headers .= "X-Priority: 1\r \n";
 
